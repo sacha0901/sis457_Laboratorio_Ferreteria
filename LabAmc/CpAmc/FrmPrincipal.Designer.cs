b60ffc58051f6a27d981c2b236a64dc723fff463
sacha0901 <asoigo0901@gmail.com>
2023-06-12 00:33:54 -0400

    Se concluyo el diseño de la interfaz de FrmFactura

--- LabAmc/CpAmc/FrmPrincipal.Designer.cs
+++ LabAmc/CpAmc/FrmPrincipal.Designer.cs
@@ -31,32 +31,33 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
             this.c1Ribbon1 = new C1.Win.Ribbon.C1Ribbon();
             this.ribbonApplicationMenu1 = new C1.Win.Ribbon.RibbonApplicationMenu();
-            this.ribbonQat1 = new C1.Win.Ribbon.RibbonQat();
-            this.ribbonConfigToolBar1 = new C1.Win.Ribbon.RibbonConfigToolBar();
-            this.ribbonTopToolBar1 = new C1.Win.Ribbon.RibbonTopToolBar();
             this.ribbonBottomToolBar1 = new C1.Win.Ribbon.RibbonBottomToolBar();
+            this.ribbonConfigToolBar1 = new C1.Win.Ribbon.RibbonConfigToolBar();
+            this.ribbonQat1 = new C1.Win.Ribbon.RibbonQat();
             this.ribbonTab1 = new C1.Win.Ribbon.RibbonTab();
             this.ribbonGroup1 = new C1.Win.Ribbon.RibbonGroup();
+            this.ribbonButton1 = new C1.Win.Ribbon.RibbonButton();
             this.ribbonTab3 = new C1.Win.Ribbon.RibbonTab();
             this.ribbonGroup3 = new C1.Win.Ribbon.RibbonGroup();
+            this.ribbonButton2 = new C1.Win.Ribbon.RibbonButton();
             this.ribbonTab4 = new C1.Win.Ribbon.RibbonTab();
             this.ribbonGroup4 = new C1.Win.Ribbon.RibbonGroup();
-            this.ribbonTab5 = new C1.Win.Ribbon.RibbonTab();
-            this.ribbonGroup5 = new C1.Win.Ribbon.RibbonGroup();
-            this.ribbonTab6 = new C1.Win.Ribbon.RibbonTab();
-            this.ribbonGroup6 = new C1.Win.Ribbon.RibbonGroup();
-            this.ribbonButton1 = new C1.Win.Ribbon.RibbonButton();
-            this.ribbonButton2 = new C1.Win.Ribbon.RibbonButton();
             this.ribbonButton3 = new C1.Win.Ribbon.RibbonButton();
             this.ribbonGroup2 = new C1.Win.Ribbon.RibbonGroup();
             this.ribbonButton4 = new C1.Win.Ribbon.RibbonButton();
             this.ribbonButton5 = new C1.Win.Ribbon.RibbonButton();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ribbonTab5 = new C1.Win.Ribbon.RibbonTab();
+            this.ribbonGroup5 = new C1.Win.Ribbon.RibbonGroup();
             this.ribbonButton6 = new C1.Win.Ribbon.RibbonButton();
             this.ribbonButton7 = new C1.Win.Ribbon.RibbonButton();
             this.ribbonButton8 = new C1.Win.Ribbon.RibbonButton();
+            this.ribbonTab6 = new C1.Win.Ribbon.RibbonTab();
+            this.ribbonGroup6 = new C1.Win.Ribbon.RibbonGroup();
             this.ribbonButton9 = new C1.Win.Ribbon.RibbonButton();
             this.ribbonButton10 = new C1.Win.Ribbon.RibbonButton();
+            this.ribbonTopToolBar1 = new C1.Win.Ribbon.RibbonTopToolBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ribbonButton11 = new C1.Win.Ribbon.RibbonButton();
             ((System.ComponentModel.ISupportInitialize)(this.c1Ribbon1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -84,21 +85,17 @@
             // 
             this.ribbonApplicationMenu1.Name = "ribbonApplicationMenu1";
             // 
-            // ribbonQat1
+            // ribbonBottomToolBar1
             // 
-            this.ribbonQat1.Name = "ribbonQat1";
+            this.ribbonBottomToolBar1.Name = "ribbonBottomToolBar1";
             // 
             // ribbonConfigToolBar1
             // 
             this.ribbonConfigToolBar1.Name = "ribbonConfigToolBar1";
             // 
-            // ribbonTopToolBar1
+            // ribbonQat1
             // 
-            this.ribbonTopToolBar1.Name = "ribbonTopToolBar1";
-            // 
-            // ribbonBottomToolBar1
-            // 
-            this.ribbonBottomToolBar1.Name = "ribbonBottomToolBar1";
+            this.ribbonQat1.Name = "ribbonQat1";
             // 
             // ribbonTab1
             // 
@@ -112,6 +109,14 @@
             this.ribbonGroup1.Name = "ribbonGroup1";
             this.ribbonGroup1.Text = "Salir de la Aplicacion";
             // 
+            // ribbonButton1
+            // 
+            this.ribbonButton1.IconSet.Add(new C1.Framework.C1BitmapIcon("DefaultImage", new System.Drawing.Size(16, 16), System.Drawing.Color.Transparent, "DefaultImage", -1));
+            this.ribbonButton1.IconSet.Add(new C1.Framework.C1BitmapIcon("Cancel2", new System.Drawing.Size(32, 32), System.Drawing.Color.Transparent, "Preset_LargeImages", 25));
+            this.ribbonButton1.Name = "ribbonButton1";
+            this.ribbonButton1.Text = "Salir";
+            this.ribbonButton1.Click += new System.EventHandler(this.ribbonButton1_Click);
+            // 
             // ribbonTab3
             // 
             this.ribbonTab3.Groups.Add(this.ribbonGroup3);
@@ -124,6 +129,14 @@
             this.ribbonGroup3.Name = "ribbonGroup3";
             this.ribbonGroup3.Text = "Administración de Catalogos";
             // 
+            // ribbonButton2
+            // 
+            this.ribbonButton2.IconSet.Add(new C1.Framework.C1BitmapIcon("DefaultImage", new System.Drawing.Size(16, 16), System.Drawing.Color.Transparent, "DefaultImage", -1));
+            this.ribbonButton2.IconSet.Add(new C1.Framework.C1BitmapIcon(null, new System.Drawing.Size(32, 32), System.Drawing.Color.Transparent, ((System.Drawing.Image)(resources.GetObject("ribbonButton2.IconSet")))));
+            this.ribbonButton2.Name = "ribbonButton2";
+            this.ribbonButton2.Text = "Articulo";
+            this.ribbonButton2.Click += new System.EventHandler(this.ribbonButton2_Click);
+            // 
             // ribbonTab4
             // 
             this.ribbonTab4.Groups.Add(this.ribbonGroup4);
@@ -134,51 +147,9 @@
             // ribbonGroup4
             // 
             this.ribbonGroup4.Items.Add(this.ribbonButton3);
+            this.ribbonGroup4.Items.Add(this.ribbonButton11);
             this.ribbonGroup4.Name = "ribbonGroup4";
             this.ribbonGroup4.Text = "Ventas";
-            // 
-            // ribbonTab5
-            // 
-            this.ribbonTab5.Groups.Add(this.ribbonGroup5);
-            this.ribbonTab5.Name = "ribbonTab5";
-            this.ribbonTab5.Text = "Administracion";
-            // 
-            // ribbonGroup5
-            // 
-            this.ribbonGroup5.Items.Add(this.ribbonButton6);
-            this.ribbonGroup5.Items.Add(this.ribbonButton7);
-            this.ribbonGroup5.Items.Add(this.ribbonButton8);
-            this.ribbonGroup5.Name = "ribbonGroup5";
-            this.ribbonGroup5.Text = "Administración";
-            // 
-            // ribbonTab6
-            // 
-            this.ribbonTab6.Groups.Add(this.ribbonGroup6);
-            this.ribbonTab6.Name = "ribbonTab6";
-            this.ribbonTab6.Text = "Ayuda";
-            // 
-            // ribbonGroup6
-            // 
-            this.ribbonGroup6.Items.Add(this.ribbonButton9);
-            this.ribbonGroup6.Items.Add(this.ribbonButton10);
-            this.ribbonGroup6.Name = "ribbonGroup6";
-            this.ribbonGroup6.Text = "Ayuda";
-            // 
-            // ribbonButton1
-            // 
-            this.ribbonButton1.IconSet.Add(new C1.Framework.C1BitmapIcon("DefaultImage", new System.Drawing.Size(16, 16), System.Drawing.Color.Transparent, "DefaultImage", -1));
-            this.ribbonButton1.IconSet.Add(new C1.Framework.C1BitmapIcon("Cancel2", new System.Drawing.Size(32, 32), System.Drawing.Color.Transparent, "Preset_LargeImages", 25));
-            this.ribbonButton1.Name = "ribbonButton1";
-            this.ribbonButton1.Text = "Salir";
-            this.ribbonButton1.Click += new System.EventHandler(this.ribbonButton1_Click);
-            // 
-            // ribbonButton2
-            // 
-            this.ribbonButton2.IconSet.Add(new C1.Framework.C1BitmapIcon("DefaultImage", new System.Drawing.Size(16, 16), System.Drawing.Color.Transparent, "DefaultImage", -1));
-            this.ribbonButton2.IconSet.Add(new C1.Framework.C1BitmapIcon(null, new System.Drawing.Size(32, 32), System.Drawing.Color.Transparent, ((System.Drawing.Image)(resources.GetObject("ribbonButton2.IconSet")))));
-            this.ribbonButton2.Name = "ribbonButton2";
-            this.ribbonButton2.Text = "Articulo";
-            this.ribbonButton2.Click += new System.EventHandler(this.ribbonButton2_Click);
             // 
             // ribbonButton3
             // 
@@ -193,7 +164,7 @@
             this.ribbonGroup2.Items.Add(this.ribbonButton4);
             this.ribbonGroup2.Items.Add(this.ribbonButton5);
             this.ribbonGroup2.Name = "ribbonGroup2";
-            this.ribbonGroup2.Text = "Consutas y busqueda";
+            this.ribbonGroup2.Text = "Consultas y busqueda";
             // 
             // ribbonButton4
             // 
@@ -201,6 +172,7 @@
             this.ribbonButton4.IconSet.Add(new C1.Framework.C1BitmapIcon(null, new System.Drawing.Size(32, 32), System.Drawing.Color.Transparent, ((System.Drawing.Image)(resources.GetObject("ribbonButton4.IconSet")))));
             this.ribbonButton4.Name = "ribbonButton4";
             this.ribbonButton4.Text = "Factura";
+            this.ribbonButton4.Click += new System.EventHandler(this.ribbonButton4_Click);
             // 
             // ribbonButton5
             // 
@@ -209,16 +181,19 @@
             this.ribbonButton5.Name = "ribbonButton5";
             this.ribbonButton5.Text = "Ventas";
             // 
-            // pictureBox1
+            // ribbonTab5
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 161);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(863, 326);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.ribbonTab5.Groups.Add(this.ribbonGroup5);
+            this.ribbonTab5.Name = "ribbonTab5";
+            this.ribbonTab5.Text = "Administracion";
+            // 
+            // ribbonGroup5
+            // 
+            this.ribbonGroup5.Items.Add(this.ribbonButton6);
+            this.ribbonGroup5.Items.Add(this.ribbonButton7);
+            this.ribbonGroup5.Items.Add(this.ribbonButton8);
+            this.ribbonGroup5.Name = "ribbonGroup5";
+            this.ribbonGroup5.Text = "Administración";
             // 
             // ribbonButton6
             // 
@@ -242,6 +217,19 @@
             this.ribbonButton8.Text = "Clientes";
             this.ribbonButton8.Click += new System.EventHandler(this.ribbonButton8_Click);
             // 
+            // ribbonTab6
+            // 
+            this.ribbonTab6.Groups.Add(this.ribbonGroup6);
+            this.ribbonTab6.Name = "ribbonTab6";
+            this.ribbonTab6.Text = "Ayuda";
+            // 
+            // ribbonGroup6
+            // 
+            this.ribbonGroup6.Items.Add(this.ribbonButton9);
+            this.ribbonGroup6.Items.Add(this.ribbonButton10);
+            this.ribbonGroup6.Name = "ribbonGroup6";
+            this.ribbonGroup6.Text = "Ayuda";
+            // 
             // ribbonButton9
             // 
             this.ribbonButton9.IconSet.Add(new C1.Framework.C1BitmapIcon("DefaultImage", new System.Drawing.Size(16, 16), System.Drawing.Color.Transparent, "DefaultImage", -1));
@@ -255,6 +243,28 @@
             this.ribbonButton10.IconSet.Add(new C1.Framework.C1BitmapIcon(null, new System.Drawing.Size(32, 32), System.Drawing.Color.Transparent, ((System.Drawing.Image)(resources.GetObject("ribbonButton10.IconSet")))));
             this.ribbonButton10.Name = "ribbonButton10";
             this.ribbonButton10.Text = "Acerca De";
+            // 
+            // ribbonTopToolBar1
+            // 
+            this.ribbonTopToolBar1.Name = "ribbonTopToolBar1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 161);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(863, 326);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ribbonButton11
+            // 
+            this.ribbonButton11.IconSet.Add(new C1.Framework.C1BitmapIcon("DefaultImage", new System.Drawing.Size(16, 16), System.Drawing.Color.Transparent, "DefaultImage", -1));
+            this.ribbonButton11.IconSet.Add(new C1.Framework.C1BitmapIcon(null, new System.Drawing.Size(32, 32), System.Drawing.Color.Transparent, ((System.Drawing.Image)(resources.GetObject("ribbonButton11.IconSet")))));
+            this.ribbonButton11.Name = "ribbonButton11";
+            this.ribbonButton11.Text = "Generar Factura";
             // 
             // FrmPrincipal
             // 
@@ -302,5 +312,6 @@
         private C1.Win.Ribbon.RibbonButton ribbonButton8;
         private C1.Win.Ribbon.RibbonButton ribbonButton9;
         private C1.Win.Ribbon.RibbonButton ribbonButton10;
+        private C1.Win.Ribbon.RibbonButton ribbonButton11;
     }
 }
